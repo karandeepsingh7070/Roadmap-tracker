dcd1e51c245f3e2b4f359e5a7bfa59d4cef94d3f
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-02-23 10:30:10 +0530

    handling build errors

--- app/Components/CourseSteps.tsx
+++ app/Components/CourseSteps.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SYLLABUS, syllabus } from '../utils/syllabus';
 import "../style.css"
 import { getUserProgress, saveUserProgress } from '../helper';
